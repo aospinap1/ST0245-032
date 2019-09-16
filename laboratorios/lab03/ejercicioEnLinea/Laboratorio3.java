c78025f6904d504e768a015658019b8b1e9ba0b4
aospinap1 <52968096+aospinap1@users.noreply.github.com>
2019-09-15 22:05:35 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/Laboratorio3.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Laboratorio3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Laboratorio3
+{
+    public static void punto1 (String str) {
+        str.replaceAll ("[", "");
+        str.replaceAll ("]", "");
+        str.replaceAll (" ", "");
+    }
+    
+}
