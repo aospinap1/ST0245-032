2bfa7e0a8d1d387a2164de3ed48df11aae8c061c
aospinap1 <52968096+aospinap1@users.noreply.github.com>
2019-09-20 16:28:53 -0500

    Update Recursión1.java

--- laboratorios/lab01/ejercicioEnLinea/Recursión1.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursión1.java
@@ -7,6 +7,48 @@
  */
 public class Recursión1
 {
+        public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return (n*factorial(n-1));
+    }
+
+    public int bunnyEars(int bunnies) {
+
+        if (0 == bunnies) {
+            return 0;
+        }
+
+        return 2 + bunnyEars (bunnies -1);
+    }
+
+    public int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fibonacci (n-1) + fibonacci (n-2); 
+    }
+
+    public int bunnyEars2(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        } else if (bunnies % 2 != 0) {
+            return 2 + bunnyEars2 (bunnies - 1);
+        } else {
+            return 3 + bunnyEars2 (bunnies - 1);
+        }
+    }
+
+    public int triangle(int rows) {
+        if (rows == 0) {
+            return 0;
+        }
+        return rows + triangle (rows - 1);
+    }
+
     public int countX(String str) {
         if (str.equals ("")) {
             return 0; 
